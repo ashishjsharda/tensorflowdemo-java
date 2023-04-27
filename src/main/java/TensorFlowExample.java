15437606af11e2f669ecf5442f9b89303dac029e
Ashish Sharda <ashishjsharda@gmail.com>
2023-04-27 07:51:11 -0500

    update class

--- src/main/java/TensorFlowExample.java
+++ src/main/java/TensorFlowExample.java
@@ -1,5 +1,34 @@
-public class TensorFlowExample {
-    public static void main(String[] args) {
+import org.tensorflow.Graph;
+import org.tensorflow.Session;
+import org.tensorflow.Tensor;
+import org.tensorflow.op.Ops;
+import org.tensorflow.op.core.Constant;
+import org.tensorflow.op.math.Add;
 
+import java.nio.charset.StandardCharsets;
+
+public class TensorFlowExample {
+    public static void main(String[] args) throws Exception {
+        // Construct the computation graph
+        try (Graph graph = new Graph()) {
+            // Create a constant tensor with a string value
+            String message = "Hello, TensorFlow!";
+            byte[] messageBytes = message.getBytes(StandardCharsets.UTF_8);
+            long[] shape = new long[] {messageBytes.length};
+            Tensor<String> inputTensor = (Tensor<String>) Tensor.create(String.class);
+            Constant<String> constant = Ops.create(graph).constant(inputTensor.bytesValue());
+
+            // Add a constant tensor to itself
+            Add<String> add = Ops.create(graph).math.add(constant.asOutput(), constant.asOutput());
+
+            // Run the computation graph in a session
+            try (Session session = new Session(graph)) {
+                Tensor<String> outputTensor = (Tensor<String>) session.runner().fetch(add.asOutput()).run().get(0);
+                byte[] outputBytes = new byte[(int) outputTensor.shape()[0]];
+                outputTensor.copyTo(outputBytes);
+                String outputMessage = new String(outputBytes, StandardCharsets.UTF_8);
+                System.out.println(outputMessage);
+            }
+        }
     }
 }
